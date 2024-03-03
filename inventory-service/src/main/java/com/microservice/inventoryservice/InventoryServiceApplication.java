aef829857ebf33adb1f2ab7c20ceb0b8ac2eccf5
Onat Kutlu <onat.kutlu@stud.uni-due.de>
2024-03-03 23:14:50 +0100

    create eureka server and loadbalancing

--- inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/InventoryServiceApplication.java
@@ -1,11 +1,7 @@
 package com.microservice.inventoryservice;
 
-import com.microservice.inventoryservice.model.Inventory;
-import com.microservice.inventoryservice.repository.InventoryRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -14,7 +10,7 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
 		return args -> {
 			Inventory inventory = new Inventory();
@@ -28,6 +24,6 @@ public class InventoryServiceApplication {
 			inventoryRepository.save(inventory);
 			inventoryRepository.save(inventory1);
 		};
-	}
+	}*/
 
 }
